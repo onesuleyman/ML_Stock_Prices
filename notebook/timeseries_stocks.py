64ced268cbf43bcfd6acd72350f4d7d6786ae517
onesuleyman <suleymanekiz92@gmail.com>
2021-01-24 10:40:52 +0100

    update coding LSTM / Stock model

--- notebook/timeseries_stocks.py
+++ notebook/timeseries_stocks.py
@@ -17,10 +17,13 @@ import os
 import numpy as np 
 import tensorflow as tf 
 from sklearn.preprocessing import MinMaxScaler
+
 #
 
 df = pd.read_csv("/Users/suleymanekiz/Desktop/personal_projects/ML_Stock_Prices/data/archive/Stocks/hpq.us.txt", delimiter=',', usecols=[ 'Date', 'Open', 'High', 'Low', 'Close'])
 print('Loaded data from the Kaggle repository')
+
+
 
 #### DATA EXPLORATION
 
@@ -31,6 +34,8 @@ print('Loaded data from the Kaggle repository')
 #print(df.info)
 #       DATE SORTED!
 
+
+
 #### DATA VISUALIZATION
 
 plt.figure(figsize = (18,9))
@@ -39,6 +44,8 @@ plt.xticks(range(0,df.shape[0],500),df['Date'].loc[::500], rotation=45)
 plt.xlabel('Date',fontsize=18)
 plt.ylabel('Mid Price', fontsize=18)
 #plt.show()#
+
+
 
 #### SPLIT DATA
 #  into training-test set
@@ -51,6 +58,8 @@ mid_prices = (high_prices+low_prices)/2.0
 #split train/test
 train_data = mid_prices[:11000]
 test_data = mid_prices[11000:]
+
+
 
 #### NORMALIZE DATA
 # Scale data between 0 and 1
@@ -75,6 +84,8 @@ train_data = train_data.reshape(-1)
 #normalize test data
 test_data = scaler.transform(test_data).reshape(-1)
 
+
+
 #### AVG. SMOOTHING
 #perform average smoothing
 #to have less raggedness of data in stock prices
@@ -87,6 +98,8 @@ for ti in range(11000):
 
 #visual and test purpose
 all_mid_data = np.concatenate([train_data, test_data], axis=0)
+
+
 
 ########### ONE DAY AHEAD PREDICTION with AVERAGING
 
@@ -149,4 +162,127 @@ plt.ylabel('Mid Price')
 plt.legend(fontsize=18)
 plt.show() #line fits perfectly (follows the label TRUE) justified by the very low MSE
 
+
+
+####### LSTM (Long Short-Term Memory model)
+
+class DataGeneratorSeq(object):
+
+    def __init__(self,prices,batch_size,num_unroll):
+        self._prices = prices
+        self._prices_length = len(self._prices) - num_unroll
+        self._batch_size = batch_size
+        self._num_unroll = num_unroll
+        self._segments = self._prices_length //self._batch_size
+        self._cursor = [offset * self._segments for offset in range(self._batch_size)]
+
+    def next_batch(self):
+
+        batch_data = np.zeros((self._batch_size), dtype=np.float32)
+        batch_labels = np.zeros((self._batch_size), dtype=np.float32)
+
+        for b in range(self._batch_size):
+            if self._cursor[b]+1>=self._prices_length:
+                self._cursor[b] = np.random.randint(0,(b+1)*self._segments)
+            
+            batch_data[b] = self._prices[self._cursor[b]]
+            batch_labels[b] = self._prices[self._cursor[b] + np.random.randint(0,5)]
+
+            self._cursor[b] = (self._cursor[b]+1)%self._prices_length
+        
+        return batch_data, batch_labels
+
+    def unroll_batches(self):
+
+        unroll_data, unroll_labels = [], []
+        init_data, init_label = None, None
+        for ui in range(self._num_unroll):
+
+            data, labels = self.next_batch()
+
+            unroll_data.append(data)
+            unroll_labels.append(labels)
+
+        return unroll_data, unroll_labels
+
+    def reset_incides(self):
+        for b in range(self._batch_size):
+            self._cursor[b] = np.random.randint(0,min((b+1)*self._segments, self._prices_length-1))
+
+dg = DataGeneratorSeq(train_data,5,5)
+u_data, u_labels = dg.unroll_batches()
+
+for ui, (dat,lbl) in enumerate(zip(u_data,u_labels)):
+    print('\n\nUnrolled index %d'%ui)
+    dat_ind = dat
+    lbl_ind = lbl
+    print('\tInputs: ', dat)
+    print('\n\tOutput: ', lbl)
+
+
+
+##### Defining Hyperparameters
+
+D = 1 #dimensionality of data
+num_unrollings = 50 #number of time steps you look into the future
+batch_size = 500 #number of samples in batch 
+num_nodes = [200,200,150] #number of hidden nodes(neurons) in each layer of deep LSTM stack
+n_layers = len(num_nodes) #number of layers
+dropout = 0.2 #dropout amount
+
+#tf.reset_default_graph() #import when running multiple times
+
+##### Defining INPUTS & OUTPUTS
+
+#input data
+train_inputs, train_outputs = [], []
+
+#unroll the input over time defining placeholders for each step
+for ui in range(num_unrollings):
+    train_inputs.append(tf.placeholder(tf.float32, shape=[batch_size,D], name='train_inputs_%d'%ui))
+    train_outputs.append(tf.placeholder(tf.float32, shape=[batch_size,1], name = 'train_outputs_%d'%ui))
+
+
+
+##### Defining Parameters of the LSTM and Regression Layer
+lstm_cells = [
+    tf.contrib.rnn.LSTMCell(num_units=num_nodes[li],
+    state_is_tuple = True, 
+    initializer= tf.contrib.layers.xavier_initializer())
+for li in range(n_layers)]
+
+drop_lstm_cells = [tf.contrib.rnn.DropoutWrapper(
+    lstm, input_keep_prob=1.0,output_keep_prob=1.0-dropout, state_keep_prob=1.0-dropout
+) for lstm in lstm_cells]
+drop_multi_cll = tf.contrib.rnn.MultiRNNCell(drop_lstm_cells)
+multi_cell = tf.contriv.rnn.MultiRNNCell(lstm_cells)
+
+w = tf.get_variable('w', shape=[num_nodes[-1], 1], initializer=tf.contrib.layers.xavier_initializer())
+b = tf.get_variable('b', initializer = tf.random_uniform([1], -0.1,0.1))
+
+
+
+############ CALCULATE LSTM OUTPUT AND FED TO REGRESSION LAYER FOR FINAL PREDICTION ###############
+
+#create cell state and hidden state variables to maintain the state of the LSTM
+c, h = [], []
+initial_state = []
+for li in range(n_layers):
+    c.append(tf.Vriable(tf.zeros([batch_size, num_nodes[li]]), trainable=False))
+    h.append(tf.Vriable(tf.zeros([batch_size, num_nodes[li]]), trainable=False))
+    initial_state.append(tf.contrib.rnn.LSTMStateTuple(c[li], h[li]))
+
+#tensor transfformations due to specific format dynamic_rnn requires in its output
+all_inputs = tf.concat([tf.expand_dims(t,0) for t in train_inputs], axis=0)
+
+#all_outputs is [seq_length, batch_size, num_nodes]
+all_lstm_outputs = tf.reshape(all_lstm_outputs, [batch_size*num_unrollings,num_nodes[-1]])
+
+all_outputs = tf.nn.xw_plus_b(all_lstm_outputs,w,b)
+
+split_outputs = tf.split(all_outputs,num_unrollings,axis=0)
+
+
+
+###### LOSS CALCULATION and OPTIMIZER
 
